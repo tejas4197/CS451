8d5d5e3ea2ab18f39e96e8625f1b6734f140af22
Tejas Shah <tejas4197@hotmail.com>
2019-02-27 19:44:31 -0500

    small change in code comment

--- CheckerBoard/Assets/Scripts/gameBoard.cs
+++ CheckerBoard/Assets/Scripts/gameBoard.cs
@@ -14,7 +14,7 @@ public class gameBoard : MonoBehaviour
     public void CreateBoard(){
         for(int i=0; i<8; i++){
             for (int j=0; j<8; j++){
-                //A red tile only occurs when (i, j) blocks are both either even or odd 
+                //A white    tile only occurs when (i, j) blocks are both either even or odd 
                 if (i % 2 != 0 && j % 2 != 0 || i % 2 == 0 && j % 2 == 0) {
                     squares [i, j] = Instantiate (redTilePrefab, new Vector2 (i, j), Quaternion.identity);
                 }
